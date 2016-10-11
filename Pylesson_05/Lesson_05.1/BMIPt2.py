9950cdebc97e41894a4b4aa5c319e0a2f9e89bac
cabulioj8165 <jacabulio@yahoo.com>
2016-10-11 10:08:31 -0700

    Pylesson_05.1 changes

---
+++ Pylesson_05/Lesson_05.1/BMIPt2.py
@@ -0,0 +1,25 @@
+start = input("this is a bmi calculator. Press enter.")
+weight = float(input("Please enter what you weigh in pounds. "))
+height = float(input("Please enter your height in inches. "))
+
+bmi = (weight/(height*height))*703
+
+condition = ("Undefined")
+
+def calcBMI(bmi, condition):
+    return bmi
+if bmi < 18.5:
+    condition = ("Underweight")
+elif bmi < 24.9:
+    condition = ("Normal")
+elif bmi < 29.9:
+    condition = ("Overweight")
+elif bmi < 34.9:
+    condition = ("Obese")
+elif bmi < 39.9:
+    condition = ("Very Obese")
+elif bmi > 39.9:
+    condition = ("Morbidly Obese")
+
+
+print("{}{}{}{:0.2f}{}".format("You are considered " , condition, ". Your bmi is " , bmi , ". " ))
--- Pylesson_05/Lesson_05.1/GPACalculator.py
+++ Pylesson_05/Lesson_05.1/GPACalculator.py
@@ -11,66 +11,16 @@ elective1b = input("Enter the grade that you have in " + elective1a + ": ")
 elective2a = input("What is the other elective that you take? ")
 elective2b = input("Enter the grade that you have in " + elective2a + ": ")
 
-def calcPoints(Math):
-    if math == "a" or "A":
+def calcPoints(grade):
+    if grade == "a":
         return 4.0
-    elif math == "b" or "B":
+    elif grade == "b":
         return 3.0
-    elif math == "c" or "C":
+    elif grade == "c":
         return 2.0
-    elif math == "d" or "D":
-        return 1.0
-    else:
-        return 0
-    if science1 == "a" or "A":
-        return 4.0
-    elif science1 == "b" or "B":
-        return 3.0
-    elif science1 == "c" or "C":
-        return 2.0
-    elif science1 == "d" or "D":
-        return 1.0
-    else:
-        return 0
-    if language1 == "a" or "A":
-        return 4.0
-    elif language1 == "b" or "B":
-        return 3.0
-    elif language1 == "c" or "C":
-        return 2.0
-    elif language1 == "d" or "D":
-        return 1.0
-    else:
-        return 0
-    if english == "a" or "A":
-        return 4.0
-    elif english == "b" or "B":
-        return 3.0
-    elif english == "c" or "C":
-        return 2.0
-    elif english == "d" or "D":
-        return 1.0
-    else:
-        return 0
-    if elective1b == "a" or "A":
-        return 4.0
-    elif elective1b == "b" or "B":
-        return 3.0
-    elif elective1b == "c" or "C":
-        return 2.0
-    elif elective1b == "d" or "D":
-        return 1.0
-    else:
-        return 0
-    if elective2b == "a" or "A":
-        return 4.0
-    elif elective2b == "b" or "B":
-        return 3.0
-    elif elective2b == "c" or "C":
-        return 2.0
-    elif elective2b == "d" or "D":
+    elif grade == "d":
         return 1.0
     else:
         return 0
 
-GPA = float((calcPoints(
+print((calcPoints(math) + calcPoints(science1) +calcPoints(pe) +calcPoints(language1) +calcPoints(elective2b) +calcPoints(english) +calcPoints(elective1b)) / 7)
