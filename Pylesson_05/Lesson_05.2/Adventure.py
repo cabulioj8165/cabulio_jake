7e2331990701fedffe390b6fc0b9ecac94338050
cabulioj8165 <jacabulio@yahoo.com>
2016-10-13 10:23:30 -0700

    Lesson_05.2 changes

--- Pylesson_05/Lesson_05.2/Adventure.py
+++ Pylesson_05/Lesson_05.2/Adventure.py
@@ -1,4 +1,5 @@
 import time
+
 def gameStart():
     gameStart = input("This is an adventure game. Tap of Y to play or N if you don't want to. ")
     if gameStart == "Y" or gameStart == "N":
@@ -7,11 +8,11 @@ def gameStart():
            time.sleep(.5)
            fname = input("Let's first ask for your name? ")
            time.sleep(.5)
-           age = int(input("Hey " + fname + ". How old are you? ")
+           age = input("Hey " + fname + ". How old are you? ")
            if age < 14:
-                print("Oh... you are too young to play this game. ")
+               print("Oh... you are too young to play this game. ")
            else:
-                print("Okay, great! ")
+               print("Okay, great! ")
        else:
             print("Aww that sucks!")
 gameStart()
