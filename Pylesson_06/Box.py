c1854e69580e81c3796d04bfef1a1a27be56ffb2
cabulioj8165 <jacabulio@yahoo.com>
2016-10-20 10:17:01 -0700

    Pylesson_06

---
+++ Pylesson_06/Box.py
@@ -0,0 +1,15 @@
+box = input("Please enter a word: ")
+output = ""
+
+for i in range(0, len(box)):
+    output = output + str(i) + " "
+for i in range(1):
+    print(box[0])
+for i in range(1):
+    print(box[1])
+for i in range(1):
+    print(box[2])
+for i in range(1):
+    print(box[3])
+for i in range(1):
+    print(box[4])
---
+++ Pylesson_06/Lesson_06.py
@@ -0,0 +1,14 @@
+output = ""
+for i in range(10, 2, -2):
+    output = output + str(i) + " "
+
+need = int(input("Please enter the number of cookies that you need: "))
+batchSize = 25
+batch = 1
+
+for cookies in range(need, -1, -batchSize):
+    print("Cookies needed: ", cookies)
+    print("Batch #: ", batch)
+    batch = batch + 1
+
+print("Order Up!!")
