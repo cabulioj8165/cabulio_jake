af09efcd13bfb9b7449ebac4afd63a1820991324
cabulioj8165 <jacabulio@yahoo.com>
2016-10-25 10:18:58 -0700

    Pylesson_06 changes

--- Pylesson_06/Lab_06.1/Song.py
+++ Pylesson_06/Lab_06.1/Song.py
@@ -1,5 +1,15 @@
-word = "Na"
-def Line():
-    for i in range(len(word)):
-        print(word[len(word):i])
-Line()
+word1 = "Na"
+word2 = "Hey"
+word3 = "Goodbye!"
+def Line(word1, word2, word3):
+    for i in range(len(word1)):
+        word1 = word1 + " "
+        print(word1)
+    for i in range(len(word2)-2):
+        word2 = word2 + " "
+        print(word2 + word2 + word2)
+    for i in range(len(word3)-7):
+        word3 = word3 + " "
+        print(word3)
+        print("Do you know the song? ")
+Line(word1, word2, word3)
