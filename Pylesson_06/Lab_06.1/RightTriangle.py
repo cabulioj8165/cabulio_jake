a9bfc7ff3f01a112c81e153381ae51c1a468cc70
cabulioj8165 <jacabulio@yahoo.com>
2016-10-27 10:07:13 -0700

    Pylesson_06 changes

--- Pylesson_06/Lab_06.1/RightTriangle.py
+++ Pylesson_06/Lab_06.1/RightTriangle.py
@@ -1,6 +1,6 @@
 word = input("Please enter a word: ")
 
 def RightTri(wrd):
-    for i in range(len(wrd), 0, -1):
+    for i in range(len(wrd)+1, 0, -1):
         print(wrd[i:len(wrd)])
 RightTri(word)
