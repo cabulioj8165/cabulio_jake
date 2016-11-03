1c4bb7f2666cf133954511175320c8481b19f174
cabulioj8165 <jacabulio@yahoo.com>
2016-11-03 09:34:47 -0700

    Pylesson_07 changes

--- Pylesson_07/ReplaceAts.py
+++ Pylesson_07/ReplaceAts.py
@@ -2,6 +2,6 @@ sentence = input("Please enter a sentence: ")
 
 top = 0
 while top < sentence.count("a") > 0:
-    sentence = sentence[0 : sentence.index("a")] + sentence[sentence.index("a")+1 : len(sentence)]
+    sentence = sentence[0 : sentence.index("a")]  + "@" + sentence[sentence.index("a")+1 : len(sentence)]
 
 print(sentence)
