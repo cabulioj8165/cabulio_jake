5184b18e7c9eba925121e5fb011e597dee1d5518
cabulioj8165 <jacabulio@yahoo.com>
2017-01-25 10:43:31 -0800

    Pylesson09

---
+++ Pylesson_08/Pylesson_09/Lesson_09 (2).py
@@ -0,0 +1,22 @@
+myList = []
+
+myList.append("a")
+myList.append(3)
+myList.append("**g")
+
+print(myList)
+
+
+myList = []
+
+for i in range(0, 8):
+    myList.append(i * 4)
+
+output = ""
+j = 0
+for i in myList:
+    output += str(i)
+    if j < len(myList):
+        output += ", "
+    j +=1
+print(myList)
